3df2db54b37150d09e1a471988ad9f7cc47181ff
phithai <phithai289@gmail.com>
2020-08-05 00:04:02 +0700

    # update file reader

--- src/BaiTapChepCode/FileReader.cpp
+++ src/BaiTapChepCode/FileReader.cpp
@@ -1,44 +1,34 @@
-#include <iostream> 
-  
+#include <iostream>  
 /* fstream header file for ifstream, ofstream,  
    fstream classes */
 #include <fstream> 
-  
 using namespace std; 
-
 // Driver Code 
 int main() 
 { 
     // Creation of fstream class object 
     fstream fio; 
     string line; 
-    fio.open("D://sample.txt", ios::trunc | ios::out | ios::in); 
+    fio.open("./src/Baitapchepcode/sample.txt", ios::trunc | ios::out | ios::in); 
     // Execute a loop If file successfully Opened 
     while (fio) { 
         // Read a Line from standard input 
         getline(cin, line); 
-  
         // Press -1 to exit 
         if (line == "-1") 
             break; 
-  
         // Write line in file 
         fio << line << endl; 
-    } d
-  
+    } 
     // Execute a loop untill EOF (End of File) 
     // point read pointer at beginning of file 
     fio.seekg(0, ios::beg); 
-  
     while (fio) { 
-  
         // Read a Line from File 
         getline(fio, line); 
-  
         // Print line in Console 
         cout << line << endl; 
     } 
-  
     // Close the file 
     fio.close(); 
   
