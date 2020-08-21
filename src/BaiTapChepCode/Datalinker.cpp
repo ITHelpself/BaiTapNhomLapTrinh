315879641c6c2d8704741d165fc0292de2a17c3f
phithai <phithai289@gmail.com>
2020-08-21 14:23:17 +0700

    #add Datalinker file

---
+++ src/BaiTapChepCode/Datalinker.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+// // typedef struct Employee{
+// //     std::string name;
+// //     float salary;
+// // };
+// class Employee{
+//     public:
+//         std::string name;
+//         float salary;
+// };
+typedef struct Node{
+    int data;
+    Node *next;
+};
+class Node{
+    public:
+        int data;
+        Node *next;
+}
+int main(){
+    
+    return 0;
+}
